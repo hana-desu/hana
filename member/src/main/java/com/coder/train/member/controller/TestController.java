817064412a02932fef68b0a07d0f343c51cc7a28
hana-desu <hanadesu233@gmail.com>
2024-07-18 00:38:09 +0800

    4.3 新增member会员模块

--- member/src/main/java/com/coder/train/member/controller/TestController.java
+++ member/src/main/java/com/coder/train/member/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.coder.train;
+package com.coder.train.member.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/hello")
     public String hello() {
-
         return "hello";
     }
 }
