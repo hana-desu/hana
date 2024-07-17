29aa5b9f89e041d436c31840d5edb4c1c15c2ebe
hana-desu <hanadesu233@gmail.com>
2024-07-17 23:02:07 +0800

    第一次初始化

---
+++ src/test/java/com/coder/train/TrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coder.train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
